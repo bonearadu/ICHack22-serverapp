347bdad04a12cb12c9eb805dd17a96ca09fb8e3b
Radu Bonea <bonearadu@gmail.com>
2022-02-05 21:13:47 +0000

    Remove user app; create game app

--- user/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class User(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=60)
