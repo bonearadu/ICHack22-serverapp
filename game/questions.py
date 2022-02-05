2ec1010f509f29db22a52f6e13ef1f5423afea56
Radu Bonea <bonearadu@gmail.com>
2022-02-05 21:15:48 +0000

    Add questions list

---
+++ game/questions.py
@@ -0,0 +1,6 @@
+questions = ["What is your favourite programming language?",
+             "What is your favourite food?",
+             "What are your favourite hobbies?",
+             "What is your university?",
+             "What technology makes the world a worse place?",
+             "Where are you from?"]
