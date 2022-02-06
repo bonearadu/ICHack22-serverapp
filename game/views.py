466553b7838361a63f86b579ebd669e686d5b2ab
Radu Bonea <bonearadu@gmail.com>
2022-02-06 01:03:22 +0000

    Remove redundant imports

--- game/views.py
+++ game/views.py
@@ -1,7 +1,3 @@
-from django.core.exceptions import BadRequest
-from django.http import HttpResponse
-from django.shortcuts import render
-
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
